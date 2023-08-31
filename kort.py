a9d5dc10ab55b081c3da132cacd97f2aa3465a11
Yasin Gul <yasing@AirsomthorYasin.lan>
2023-08-31 22:33:10 +0200

    Pusharr en mer objekt orienterad lösnig

--- kort.py
+++ kort.py
@@ -2,3 +2,16 @@ import random
 
 class Kort:
     def __init__(self, färg, rank):
+        self.färg = färg
+        self.rank = rank
+        if rank in ['Knekt', 'Dam', 'Kung']:
+            self.värde = 10
+        elif rank == 'Ess':
+            self.värde = 11
+        else:
+            self.värde = int(rank)
+
+    def __str__(self):
+        return f"{self.rank} av {self.färg}" # Kort
+
+    class kortlet
